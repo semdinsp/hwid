8eba07dee1736a05d8f2fef7107af6ad6296639f
semdinsp <scott.sproule@ficonab.com>
2019-07-15 08:28:05 +0800

    update for simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start
 puts "in test helper"
 require 'rubygems'
 require 'bundler/setup'
@@ -6,11 +8,6 @@ CodeClimate::TestReporter.start
 require 'stringio'
 require 'minitest/autorun'
 require 'minitest/unit'
-SimpleCov.command_name 'test'
-#SimpleCov.profiles.define 'mygem' do
-#  add_group "Gem", '/lib/' # additional config here
-#end
-SimpleCov.start 
 
 require File.dirname(__FILE__) + '/../lib/hwid'
 
